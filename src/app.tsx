821a3b8ad5f5d744c203d3378496954903331b3d
Yao <kaiser1644@gmail.com>
2021-01-25 21:54:09 -0500

    Theme and menu

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,35 @@
 import * as React from "react";
+import styled, { ThemeProvider } from 'styled-components';
+import {} from 'styled-components/cssprop';
 import {
     createMuiTheme,
-    ThemeProvider,
+    ThemeProvider as MuiThemeProvider
 } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { blue, indigo } from '@material-ui/core/colors'
+import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
+import { blueGrey } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
     palette: {
+        primary: {
+            main: blueGrey[900]
+        },
+        /*
         secondary: {
             main: blue[900]
         },
-        primary: {
-            main: indigo[700]
-        },
+        */
         type: "dark"
     },
     typography: {
-    // Use the system font instead of the default Roboto font.
         fontFamily: [
             '"Lato"',
             'sans-serif'
@@ -27,18 +37,53 @@ const theme = createMuiTheme({
     }
 });
 
+const StyledAppBar = styled(AppBar)`
+    ${({ theme }) => `z-index: ${theme.zIndex.drawer+1}`}
+`;
+
+const StyledDrawer = styled(({ width, ...other }) => (
+    <Drawer classes={{ paper: 'paper' }} {...other} />
+  ))`
+    width: 240,
+    flexShrink: 0,
+    & .paper {
+        width: 240
+    }
+`;
+
+const StyledDrawerDiv = styled.div`
+    overflow: 'auto'
+`;
+
 class App extends React.Component {
     render() {
         return (
+            <MuiThemeProvider theme={theme}>
             <ThemeProvider theme={theme}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography>
-                            Derivatives Visualization
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                <CssBaseline>
+                    <StyledAppBar position="fixed">
+                        <Toolbar>
+                            <Typography variant="h4">
+                                Derivatives Visualizer
+                            </Typography>
+                        </Toolbar>
+                    </StyledAppBar>
+                    <StyledDrawer variant="permanent">
+                        <Toolbar />
+                        <StyledDrawerDiv>
+                            <List>
+                                {['Basic', 'Spreads', 'Advanced', 'Custom'].map(text => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon><AssessmentOutlinedIcon /></ListItemIcon>
+                                    <ListItemText><Typography variant="h6">{text}</Typography></ListItemText>
+                                </ListItem>
+                                ))}
+                            </List>
+                        </StyledDrawerDiv>
+                    </StyledDrawer>
+                </CssBaseline>
             </ThemeProvider>
+            </MuiThemeProvider>
         );
     }
 }
